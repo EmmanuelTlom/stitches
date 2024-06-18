63fd3556594b17cc8e501203697ec5a74da8622d
emmanueltlom <nwabuodafiemmanuel@gmail.com>
2024-06-19 00:07:19 +0100

    landing pages

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     type: "loop",
     autoplay: true,
     arrows: false,
+    pagination: false,
     interval: 3000,
   }).mount();
 });
@@ -11,16 +12,29 @@ document.addEventListener("DOMContentLoaded", function () {
     type: "loop",
     autoplay: true,
     arrows: false,
+    pagination: false,
     interval: 5000,
   }).mount();
 });
 document.addEventListener("DOMContentLoaded", function () {
   new Splide("#splide3", {
-    type: "loop",
     autoplay: true,
+    rewind: true,
     perPage: 3,
+    pagination: false,
     perMove: 1,
-    gap: 30,
+    gap: 20,
     arrows: true,
+    breakpoints: {
+      1200: {
+        perPage: 3,
+      },
+      768: {
+        perPage: 1,
+      },
+      480: {
+        perPage: 1,
+      },
+    },
   }).mount();
 });
