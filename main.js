2d7f4d1fbd17af6dc1118be1798bea2a98d8d803
emmanueltlom <nwabuodafiemmanuel@gmail.com>
2024-06-19 12:15:40 +0100

    landing pages

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   new Splide("#splide", {
     type: "loop",
-    autoplay: true,
+    autoplay: false,
     arrows: false,
     pagination: false,
     interval: 3000,
@@ -10,10 +10,10 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
   new Splide("#splide2", {
     type: "loop",
-    autoplay: true,
+    autoplay: false,
     arrows: false,
     pagination: false,
-    interval: 5000,
+    interval: 3000,
   }).mount();
 });
 document.addEventListener("DOMContentLoaded", function () {
@@ -34,6 +34,28 @@ document.addEventListener("DOMContentLoaded", function () {
       },
       480: {
         perPage: 1,
+      },
+    },
+  }).mount();
+});
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#splide4", {
+    autoplay: true,
+    rewind: true,
+    perPage: 6,
+    pagination: false,
+    perMove: 1,
+    gap: 20,
+    arrows: true,
+    breakpoints: {
+      1200: {
+        perPage: 6,
+      },
+      // 768: {
+      //   perPage: 1,
+      // },
+      480: {
+        perPage: 5,
       },
     },
   }).mount();
